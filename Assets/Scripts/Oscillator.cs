4d3430072d46f457cb378844b0ba0b3ff2c8f851
mwm13-beep <mwm13ca@hotmail.com>
2024-01-13 20:04:12 -0600

    Lights, 'Over' make over and moving obstacles
    Added point lights to the rocket to make it stand out more and to have the radius of the lights act as an early warning system when getting to close to obstacles. Added a glow effect to the launch and landing pads through the emission characteristic on their material. Did a visual redesign of the 'Over' level changing up the colour scheme and making it darker to give it more of a space feel. Also, added lights to 'Over' to mark places the player needs to be careful of. Lastly, wrote a script that makes obstacles move around the screen using the sin function.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    const float tau = Mathf.PI * 2;
+    
+    [SerializeField] float period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; //grows continually from 0
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        //locks range to 0 to 1 instead of -1 to 1
+        movementFactor = (rawSinWave + 1f)/2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
